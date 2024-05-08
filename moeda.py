9cf1f221ca911b9915e3994bad758da9c2ce0d19
guilhermetborges <112335378+guilhermetborges@users.noreply.github.com>
2024-05-08 14:17:55 -0300

    Add files via upload

---
+++ moeda.py
@@ -0,0 +1,18 @@
+def metade(num):
+    metade = num/2
+    return metade
+
+def dobro(x):
+    dobro = x*2
+    return dobro
+
+def aumenta10(p,Y):
+    aumento =  ((p/100)*Y)+p
+    return aumento
+
+def reduz13(w,x):
+    termo = x/100
+    redução = 1-termo
+    reduz = w*redução
+    return reduz
+    
